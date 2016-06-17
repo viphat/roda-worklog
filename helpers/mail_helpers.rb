688efe74b21ef94cf28e2a66e7419ba607fa1704
Eddie Yang <viphat@gmail.com>
2016-06-17 09:39:17 +0700

    Fix Daily Report Bugs

--- helpers/mail_helpers.rb
+++ helpers/mail_helpers.rb
@@ -1,8 +1,8 @@
 module MailHelpers
   def get_users_logs
-    @begin_date = Time.now.beginning_of_day
-    @end_date = Time.now.end_of_day
-    @date_str = Time.now.beginning_of_day.strftime("%d-%m-%Y")
+    @begin_date = (Time.now - 1.day).beginning_of_day
+    @end_date = (Time.now - 1.day).end_of_day
+    @date_str = @begin_date.strftime("%d-%m-%Y")
     team = Team.order(:id).first
     @users = User.where(team_id: team.id).order(:id).all
   end
